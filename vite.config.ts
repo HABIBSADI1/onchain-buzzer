1588f9cc69000c10e8dffdd7c43c32e8eb82e7d2
habibsadi1 <habib.sadi1990@gmail.com>
2025-07-18 21:04:15 +0330

    Final clean refactor: fix TS errors, wagmi update, game timer & click UX

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
+      buffer: 'buffer',
+      process: 'process',
     },
   },
   optimizeDeps: {
-    include: ['buffer', 'process', 'util', 'stream'],
+    include: ['buffer', 'process'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
       },
       plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
+        NodeGlobalsPolyfillPlugin({ process: true, buffer: true }),
         NodeModulesPolyfillPlugin(),
       ],
     },
   },
   define: {
     global: 'globalThis',
-  }
+  },
 })
