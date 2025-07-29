c82a49d0f8d9b814e0debd3c9396724c1509af32
HABIBSADI1 <habib.sadi1990@gmail.com>
2025-07-29 23:10:03 +0330

    chore: sync full local project to GitHub

--- frame-server/routes/frame-image.ts
+++ frame-server/routes/frame-image.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { createCanvas } from 'canvas';
 import { createPublicClient, getContract, http } from 'viem';
 import { base } from 'viem/chains';
-import abi from '../abi.json';
+import abi from './abi.json';
 
 const router = Router();
 
